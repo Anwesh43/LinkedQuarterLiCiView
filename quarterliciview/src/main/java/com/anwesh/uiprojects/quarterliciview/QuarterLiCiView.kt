e52d94e0b416048ce4d168867219eb981960bcc1
Anwesh43 <anweshthecool0@gmail.com>
2018-12-12 10:34:53 +0530

    adding libraries and defining constants

--- quarterliciview/src/main/java/com/anwesh/uiprojects/quarterliciview/QuarterLiCiView.kt
+++ quarterliciview/src/main/java/com/anwesh/uiprojects/quarterliciview/QuarterLiCiView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.quarterliciview
 /**
  * Created by anweshmishra on 12/12/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val color : Int = Color.parseColor("#0D47A1")
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.6f
