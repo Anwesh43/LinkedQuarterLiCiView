e380700744648b08fd5487819bcb38feac42e187
Anwesh43 <anweshthecool0@gmail.com>
2018-12-12 10:55:16 +0530

    adding QuarterLICI class

--- quarterliciview/src/main/java/com/anwesh/uiprojects/quarterliciview/QuarterLiCiView.kt
+++ quarterliciview/src/main/java/com/anwesh/uiprojects/quarterliciview/QuarterLiCiView.kt
@@ -163,7 +163,30 @@ class QuarterLiCiView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class QuarterLiCi(var i : Int) {
+        private var dir : Int = 1
+
+        private var curr : QLCNode = QLCNode(0)
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
