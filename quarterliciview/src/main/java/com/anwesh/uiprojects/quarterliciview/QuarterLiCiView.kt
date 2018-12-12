03424fe331e2f49e7a7e0f27bf0b31a6ff9ea029
Anwesh43 <anweshthecool0@gmail.com>
2018-12-12 10:44:46 +0530

    adding drawQLCNode canvas extension methods

--- quarterliciview/src/main/java/com/anwesh/uiprojects/quarterliciview/QuarterLiCiView.kt
+++ quarterliciview/src/main/java/com/anwesh/uiprojects/quarterliciview/QuarterLiCiView.kt
@@ -20,6 +20,7 @@ val scDiv : Double = 0.51
 val color : Int = Color.parseColor("#0D47A1")
 val strokeFactor : Int = 90
 val sizeFactor : Float = 2.6f
+val qcdeg : Float = 75f
 
 fun Int.getInverse() : Float = 1f / this
 
@@ -29,4 +30,30 @@ fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInverse() + scaleFactor() * b.getInverse()
 
-fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * scGap * dir 
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * scGap * dir
+
+fun Canvas.drawQLCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = color
+    paint.style = Paint.Style.STROKE
+    val size : Float = gap / sizeFactor
+    val lSize : Float = size / 4
+    val startDeg = (90f - qcdeg) / 2
+    save()
+    translate(gap * (i + 1), h/2)
+    rotate(90f * sc2)
+    for (j in 0..(lines - 1)) {
+        val scj : Float = sc1.divideScale(j, lines)
+        val scj1 : Float = scj.divideScale(0, 2)
+        val scj2 : Float = scj.divideScale(1, 2)
+        drawLine(size - lSize * scj1, 0f, size + lSize * scj2, 0f, paint)
+        drawArc(RectF(-size, -size, size, size), startDeg, qcdeg * scj2, false, paint)
+    }
+    restore()
+}
