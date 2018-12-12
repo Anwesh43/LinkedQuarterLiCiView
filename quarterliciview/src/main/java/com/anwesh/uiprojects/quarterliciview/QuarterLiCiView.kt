b644c6f5385a005bf226270898e0a1c451d12092
Anwesh43 <anweshthecool0@gmail.com>
2018-12-12 10:47:02 +0530

    adding QuarterLICiView class

--- quarterliciview/src/main/java/com/anwesh/uiprojects/quarterliciview/QuarterLiCiView.kt
+++ quarterliciview/src/main/java/com/anwesh/uiprojects/quarterliciview/QuarterLiCiView.kt
@@ -57,3 +57,21 @@ fun Canvas.drawQLCNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class QuarterLiCiView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
