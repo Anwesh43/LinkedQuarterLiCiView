fe714ab3807934bd31f70b02b9587983b083f7ab
Anwesh43 <anweshthecool0@gmail.com>
2018-12-13 11:48:25 +0530

    adding OnAnimationCompleteListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedquarterliciview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedquarterliciview/MainActivity.kt
@@ -3,14 +3,20 @@ package com.anwesh.uiprojects.linkedquarterliciview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.quarterliciview.QuarterLiCiView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        QuarterLiCiView.create(this)
+        val view : QuarterLiCiView = QuarterLiCiView.create(this)
         fullScreen()
+        view.addOnCompleteListener({createToast("animation number $it is complete")}, {createToast("animation number $it is reset")})
+    }
+
+    private fun createToast(txt : String) {
+        Toast.makeText(this, txt, Toast.LENGTH_SHORT).show()
     }
 }
 
